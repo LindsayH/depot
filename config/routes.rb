215eb967050bcad14b5f7ab5a45f6d49f0aa96fe
Lindsay Hammon <lindsay.work.mail@gmail.com>
2014-12-06 19:36:20 -0700

    formated the home page and added the top and side baners

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'store/index'
-
+  get "store/index"
   resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -8,6 +7,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'store#index' 
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
