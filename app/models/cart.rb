951c20dfd5a87a7a2e1cdea85603bcaf0fd8f67b
Lindsay Hammon <lindsay.work.mail@gmail.com>
2014-12-09 21:14:14 -0700

    on to chapter 11

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,8 +1,6 @@
 class Cart < ActiveRecord::Base
 	has_many :line_items, dependent: :destroy
 
-	
-
 	def add_product(product_id)
 		current_item = line_items.find_by(product_id: product_id) 
 		if current_item
@@ -13,5 +11,7 @@ class Cart < ActiveRecord::Base
   	current_item
 	end
 
-
+	def total_price
+		line_items.to_a.sum { |item| item.total_price }
+	end
 end
